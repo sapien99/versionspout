e9609ee735e829d97384ccc22829ba1eb3771559
Michael Ferjancic <michael.ferjancic@gmail.com>
2019-03-17 11:46:15 +0100

    -Add: updated curl tests

---
+++ test/curl/get_notifications.sh
@@ -0,0 +1,1 @@
+curl -v localhost:3000/profiles/michael.ferjancic@gmail.com/notifications -H 'Content-Type: application/json'
--- test/curl/inquireversionsnews.sh
+++
@@ -1,1 +0,0 @@
-curl -v localhost:3000/profiles/michael.ferjancic@gmail.com/docker-news -H 'Content-Type: application/json'
